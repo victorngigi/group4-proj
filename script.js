9e5d16f1a4c069e652fdb547202148d275c4b025
Victor Kinyanjui Ngigi <victor.ngigi@outlook.com>
2025-03-20 12:32:16 +0300

    added clear and edit button

--- script.js
+++ script.js
@@ -5,15 +5,26 @@ document.addEventListener('DOMContentLoaded', () =>{
         buildCart(e.target.cartItem.value)
         form.reset()
     })
-})
+});
 
 function buildCart(list){
     let item = document.createElement('li')
-    let btn = document.createElement('button')
-    btn.addEventListener('click', handleDone)
-    btn.textContent = 'Purchased'
+
+    let addBtn = document.createElement('button')
+    let clearBtn = document.querySelector('#clear')
+    let editBtn = document.createElement('button')
+
+    clearBtn.addEventListener('click', handleClear)
+    addBtn.addEventListener('click', handleDone)
+    editBtn.addEventListener('click', handleEdit)
+
+    addBtn.textContent = 'Purchased'
+    editBtn.textContent = 'edit'
+
     item.textContent = `${list} `
-    item.appendChild(btn)
+    item.appendChild(addBtn)
+    item.appendChild(editBtn)
+
     console.log(item)
     document.querySelector('#shoppingList').appendChild(item)
 }
@@ -22,4 +33,14 @@ function handleDone(e){
     let item = e.target.parentElement;
     item.style.textDecoration = 'line-through';
     item.style.backgroundColor = 'red';
+}
+
+function handleClear(e){
+    document.querySelector('#shoppingList').innerHTML = '';
+}
+
+function handleEdit(e){
+    let item = e.target.parentElement;
+    let newText = prompt('Edit your item:', item.firstChild.textContext)
+    item.firstChild.textContent = newText;
 }
